1b0088360f832f78b556b703cd307aa929dce559
Tristan Norton <tristan.m.norton@gmail.com>
2017-07-13 21:57:55 -0400

    Now we're rocking. Found a data source of the top 1000 most populated cities and used that instead. Updated the variables to reflect its specificity as well for readability. Added comments because I'm trying to get in the habit of doing it. I also have setup a function that returns the top restaurants in the area selected sorted by rating. The last function I need to create is to isoloate the top 5 of those and populate the DOM with them. After that I want to style it and make it look super sweet.

--- script.js
+++ script.js
@@ -1,34 +1,80 @@
+// Global Variables
 var JSONdata;
-var countryAmount;
+var cityAmount;
 var lati = 0;
 var longi = 0;
-var randomCapitalPre = 0;
-var randomCapital = 0; 
+var randomCityPre = 0;
+var randomCity = 0;
+var btn = document.getElementById('btn');
+var map;
+var service;
+var infowindow;
 
+// Extracts information of the top 1000 most populated cities in the US
 $(document).ready(function(){
     console.log('@ready');
-    $.getJSON('countries.json', function(data) {
+    $.getJSON('cities.json', function(data) {
         JSONdata = data;
-        countryAmount = JSONdata.length;
+        cityAmount = JSONdata.length;
     });
 });
 
-document.getElementById('btn').addEventListener("click", function() {
-    randomCapitalPre = Math.random() * ((countryAmount - 1) - 0) + 0;
-    randomCapital = Math.round(randomCapitalPre);
-    lati = JSONdata[randomCapital].latlng[0];
-    longi = JSONdata[randomCapital].latlng[1];
+// Randomly selects city on button click and re-initalizes map
+btn.addEventListener("click", function() {
+    randomCityPre = Math.random() * ((cityAmount - 1) - 0) + 0;
+    randomCity = Math.round(randomCityPre);
+    lati = JSONdata[randomCity].latitude;
+    longi = JSONdata[randomCity].longitude;
     initMap();
 });
 
+// Map functionality
 function initMap() {
-        var newPlace = {lat: lati, lng: longi};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 7,
-          center: newPlace
-        });
+    var newPlace = {lat: lati, lng: longi};
+    
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: newPlace
+    });
+    
+    var marker = new google.maps.Marker({
+        position: newPlace,
+        map: map
+    });
+    
+    var request = {
+        location: newPlace,
+        radius: '500',
+        query: 'Best Restaurants'
+    };
+   
+    service = new google.maps.places.PlacesService(map);
+    service.textSearch(request, callback);
+    
+    function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            // sorts returned results in order of rading
+            results.sort(function(a,b) {
+                return b.rating - a.rating;
+            });
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+            console.log(results[i].name + results[i].rating);
+          }
+        }
+    }
+
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
         var marker = new google.maps.Marker({
-          position: newPlace,
-          map: map
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
         });
       }
+}
+
